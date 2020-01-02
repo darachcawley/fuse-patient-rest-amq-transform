f1f94e1a0b773b06eadf1c3773b97c48df9420ab
darachcawley <darachcawley@gmail.com>
2020-01-02 15:16:32 +0000

    marshalling object to xml & adding to queue

--- core/inbound/src/main/java/com/redhat/usecase/route/RouteProcessor.java
+++ core/inbound/src/main/java/com/redhat/usecase/route/RouteProcessor.java
@@ -11,15 +11,15 @@ public class RouteProcessor extends RouteBuilder{
 		// catch all exception handler
 		onException(Exception.class)
 	        .to("log:fail")
-	        .handled(true);
+			.handled(true);
 		
 		//the inbound route
 		from("direct:integrateRoute")
 	       	.log(LoggingLevel.INFO, "com.usecase.route.RouteProcessor", "Route: direct:integrateRoute :: Body: ${body}")
-	       	.setExchangePattern(ExchangePattern.InOnly)
-	       	.to("direct:marshallXml")
-//	       	.to("activemq:queue:q.empi.deim.in")
-	       	.log(LoggingLevel.INFO, "com.usecase.route.RouteProcessor", "Route: direct:integrateRoute :: Marshalled XML: ${body}")
+			.setExchangePattern(ExchangePattern.InOnly)
+			.marshal("personFormat")
+	       	.to("activemq:queue:q.empi.deim.in")
+	       	.log(LoggingLevel.INFO, "com.usecase.route.RouteProcessor", "Route: direct:integrateRoute :: Marshalled XML & added to Queue: ${body}")
 	       	.transform(constant(2))
 	       	.end()
        ;
